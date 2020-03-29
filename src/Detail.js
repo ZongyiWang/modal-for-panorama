c60ccd3734d373865a6e5b75b402b61ddc8e4c16
Zongyi Wang <zwang195@illinois.edu>
2020-03-29 03:56:04 -0700

    2nd
    2nd

--- src/Detail.js
+++ src/Detail.js
@@ -1,57 +1,76 @@
 import React from "react";
 import "./styles.css";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-export default function Detail() {
-  return (
-    <div class="container">
-      <h2>Modal</h2>
-      <button
-        type="button"
-        class="btn btn-primary"
-        data-toggle="modal"
-        data-target="#detailModal"
-      >
-        Open modal
-      </button>
-      <div class="modal fade show" id="detailModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Steering wheel</h4>
-              <button type="button" class="close" data-dismiss="modal">
-                ×
-              </button>
-            </div>
-            <div class="modal-body container">
-              <div class="row">
-                <div class="col-md-10 offset-md-1">
-                  <img
-                    class="detailImage"
-                    src="https://loremflickr.com/cache/resized/65535_49446817897_9ce434b2e8_c_800_240_g.jpg"
-                    alt="random"
-                  />
+const styles = theme => ({
+  button: {
+    fontSize: "12px",
+    // margin: theme.spacing.unit,
+    minWidth: 350
+  }
+});
+
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div class="container">
+        <h2>Modal</h2>
+
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-toggle="modal"
+          data-target="#detailModal"
+        >
+          Open modal
+        </button>
+        <div class="modal fade" id="detailModal">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">{this.props.title}</h4>
+                <button type="button" class="close" data-dismiss="modal">
+                  &times;
+                </button>
+              </div>
+
+              <div class="modal-body container">
+                <div class="row">
+                  <div class="col-md-10 offset-md-1">
+                    <img
+                      class="detailImage"
+                      src={this.props.imageSrc}
+                      alt="random"
+                    />
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-10 offset-md-1">
+                    <p>{this.props.description}</p>
+                  </div>
                 </div>
               </div>
-              <div class="row">
-                <div class="col-md-10 offset-md-1">
-                  this is description this is description this is description
-                  this is description this is description this is description
-                  this is description this is description this is description{" "}
-                </div>
+
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Next
+                </button>
               </div>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Next
-              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default withStyles(styles)(Detail);
--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import ReactDOM from "react-dom";
 import Detail from "./Detail";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Detail />, rootElement);
+ReactDOM.render(
+  <Detail
+    title="Steering wheel"
+    imageSrc="https://loremflickr.com/cache/resized/65535_49446817897_9ce434b2e8_c_800_240_g.jpg"
+    description="this is description this is description this is description this is description this is description this is description this is description "
+  />,
+  rootElement
+);
